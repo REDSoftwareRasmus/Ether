14952b4454ad5a81eeecb00b1ce22ff46b5f1cbe
REDSoftwareRasmus <appdev@redge.se>
2021-02-06 19:08:52 +0100

    Documentation

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -6,25 +6,30 @@ pragma solidity >=0.4.2;
 
 
 contract Election {
-    
+
+    // Models    
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
 
-    // Read/write Candidates
+    // State
     mapping(uint => Candidate) public candidates;
     mapping(address => bool) public voters;
     
     uint public candidatesCount;
 
-    // Constructor
+    // Events
+    event votedEvent(uint indexed _candidateID); 
+
+    // Init
     constructor() public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
 
+    // Actions
     function addCandidate(string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
@@ -35,6 +40,6 @@ contract Election {
         require(_candidateID > 0 && _candidateID <= candidatesCount);
         voters[msg.sender] = true;
         candidates[_candidateID].voteCount ++;              
+        emit votedEvent(_candidateID);
     }
-
 }
