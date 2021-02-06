efa9ae57ad603f021881acb71c6a3f2e73d34eda
REDSoftwareRasmus <appdev@redge.se>
2021-02-06 18:31:26 +0100

    Add voting functionality and tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.2;
 
+
+
+
+
 contract Election {
     
     struct Candidate {
@@ -11,6 +15,8 @@ contract Election {
 
     // Read/write Candidates
     mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+    
     uint public candidatesCount;
 
     // Constructor
@@ -19,8 +25,16 @@ contract Election {
         addCandidate("Candidate 2");
     }
 
-    function addCandidate (string memory _name) private {
+    function addCandidate(string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }   
+
+    function vote(uint _candidateID) public {
+        require(!voters[msg.sender]);
+        require(_candidateID > 0 && _candidateID <= candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateID].voteCount ++;              
     }
+
 }
